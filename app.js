acc55fe32b408cde7087d84ce5c353f7d0ac448a
maxii73 <athousandsuns73@gmail.com>
2022-06-21 17:40:50 -0300

    estructura levantada

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Server running in http://localhost+${port}`));
+
+app.get ('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+
+
+
+
